3dce7d05a1e0e560a2eabb8df959d9d7d3975062
elabusch021 <elabusch021@student.wethinkcode.co.za>
2022-11-02 14:10:21 +0200

    Removed Platform Display

--- Lyzer-Scraper.py
+++ Lyzer-Scraper.py
@@ -5,25 +5,12 @@ This is module acts as the main entry point for our web scraper program.
 # System imports
 import sys
 
-# Third party imports
-import PyInstaller
-
 def main():
     """
     This is the main function for our web scraper program.
     """
     print("Scraper is running...") # Print to console that scraper is running
-    pyinstaller_details() # Print PyInstaller details to console
     return 0 # Exit code 0
-
-def pyinstaller_details():
-    """
-    This function prints the PyInstaller details to the console.
-    """
-    print("\nPYINSTALLER")
-    print("PLATFORM ->", PyInstaller.PLATFORM)
-    print("VERSION", PyInstaller.__version__)
-    print("")
 
 if __name__ == "__main__":
     exit_code = main() # Call main function
