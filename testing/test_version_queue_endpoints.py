87bfbec237a2441ff570f96271c2fdf64baae399
Evanlab02 <evanlabuschagne70@gmail.com>
2023-03-03 13:19:14 +0200

    Updated version tag

--- testing/test_version_queue_endpoints.py
+++ testing/test_version_queue_endpoints.py
@@ -27,7 +27,7 @@ class TestApiEndpointsV1(unittest.TestCase):
         "status": 200,
         "result": "success",
         "message": "Data retrieved successfully.",
-        "data": "0.9.0"
+        "data": "0.9.1"
         })
 
     def test_queue_endpoint(self):
@@ -67,5 +67,5 @@ class TestApiEndpointsV1(unittest.TestCase):
         "status": 200,
         "result": "success",
         "message": "Data retrieved successfully.",
-        "data": "0.9.0"
+        "data": "0.9.1"
         }, 200))
